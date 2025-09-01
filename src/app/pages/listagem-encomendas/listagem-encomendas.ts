ce3e12b4c6f79e7a3b4433fa585ec89fa587b385
Sérgio da Silva Junior <sergioda.silva1992@gmail.com>
2025-09-01 08:28:28 -0300

    Adicionando barra de pesquisa e estilizando

--- src/app/pages/listagem-encomendas/listagem-encomendas.ts
+++ src/app/pages/listagem-encomendas/listagem-encomendas.ts
@@ -1,31 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { EncomendaService } from '../../services/encomenda.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-
 
 @Component({
   selector: 'app-listagem-encomendas',
   templateUrl: './listagem-encomendas.html',
   styleUrls: ['./listagem-encomendas.css'],
+  standalone: true,
   imports:[FormsModule, CommonModule]
 })
 export class ListagemEncomendasComponent implements OnInit {
   encomendas: any[] = [];
-  spnErro: any;
+  encomendasOriginais: any[] = [];
+  blocoPesquisa: string = '';
+  apartamentoPesquisa: string = '';
+
+  @ViewChild('spnErro') spnErro!: ElementRef;
 
   constructor(private service: EncomendaService) {}
 
   ngOnInit() {
     this.service.listarEncomendas().subscribe((dados: any[]) => {
       this.encomendas = dados;
+      this.encomendasOriginais = dados;
     });
   }
 
   retirar(e: any) {
     this.service.marcarRetirada(e.id);
     this.exibirMensagem('Encomenda marcada como retirada!', 'success');
+  }
+
+  buscarEncomendas() {
+    this.encomendas = this.encomendasOriginais.filter(e => {
+      const bloco = this.blocoPesquisa.toLowerCase();
+      const apartamento = this.apartamentoPesquisa.toLowerCase();
+
+      const matchBloco = !bloco || e.bloco.toLowerCase().includes(bloco);
+      const matchApartamento = !apartamento || e.apartamento.toLowerCase().includes(apartamento);
+
+      return matchBloco && matchApartamento;
+    });
   }
 
   exibirMensagem(mensagem: string, tipo: 'erro' | 'success' = 'erro'): void {
@@ -36,7 +52,7 @@ export class ListagemEncomendasComponent implements OnInit {
 
     if (tipo === 'success') {
       spn.classList.add('sucesso');
-      setTimeout(() => (spn.style.display = 'none'), 8000);
+      setTimeout(() => (spn.style.display = 'none'), 5000);
     } else {
       spn.classList.add('erro');
       setTimeout(() => (spn.style.display = 'none'), 4000);
